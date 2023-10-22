3d23e065ff8722a9fa0cb7ac31ec6b15b56e9995
Павел Филиппов <pgphil@yandex.ru>
2023-10-22 03:02:23 +0300

    try

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 # DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
-ALLOWED_HOSTS = ['158.160.67.222', '127.0.0.1', 'localhost', 'food.viewdns.net']
+ALLOWED_HOSTS = ['158.160.67.222', '127.0.0.1:8000', 'localhost', 'food.viewdns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
