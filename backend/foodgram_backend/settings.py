369afa34e584829952f49e024801fc4a9d70e7ef
Павел Филиппов <pgphil@yandex.ru>
2023-10-22 19:03:58 +0300

    try

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,16 +1,11 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = True
-# DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
 ALLOWED_HOSTS = ['158.160.67.222', '127.0.0.1:8080', 'localhost', 'food.viewdns.net']
 
