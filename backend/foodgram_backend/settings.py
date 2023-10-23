0506488c12f329e7584b015810ba6331a42336b9
Павел Филиппов <pgphil@yandex.ru>
2023-10-23 13:14:27 +0300

    First working version.

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -56,26 +56,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
-#        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-#        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-#        'HOST': os.getenv('DB_HOST', ''),
-#        'DB_PORT': os.getenv('DB_PORT', '5432'),
-#    }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'foodgram',
-        'USER': 'foodgram_user',
-        'PASSWORD': 'foodgram_password',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'DB_PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'foodgram',
+#        'USER': 'foodgram_user',
+#        'PASSWORD': 'foodgram_password',
+#        'HOST': 'localhost',
+#        'PORT': 5432,
+#    }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
