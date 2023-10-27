d5f60e5ed68755bea9f9a74967c4899272c33a40
Павел Филиппов <pgphil@yandex.ru>
2023-10-27 11:31:32 +0300

    Second review. Change ingredient model.

--- backend/api/views.py
+++ backend/api/views.py
@@ -161,8 +161,14 @@ class RecipeViewSet(ModelViewSet):
             return Response(ShoppingCartSerializer(shopping_list, context={
                 'request': request,
             }).data, status=status.HTTP_201_CREATED)
-        ShoppingCart.objects.get(user=request.user, recipe_id=pk).delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        shopping_cart = ShoppingCart.objects.get(user=request.user,
+                                                 recipe_id=pk)
+        if not shopping_cart:
+            return Response({'message': 'Object not found.'},
+                            status=status.HTTP_404_NOT_FOUND)
+        else:
+            shopping_cart.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=False, methods=['GET'],
             permission_classes=[permissions.IsAuthenticated])
